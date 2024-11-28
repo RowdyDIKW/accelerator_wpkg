18b1260fa351bd38ae3322cab323eca52aabe7b0
RowdyDIKW <rowdy.van.frederikslust@dikw.com>
2024-11-28 09:10:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cbs',
+    name='dikw-accelerator',
     version='0.5',
     packages=find_packages(where='lib'),
     package_dir={'': 'lib'},
