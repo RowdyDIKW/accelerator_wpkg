8a6bc9129a0329e4c4689655c7bd380540c7e9fa
Rowdyvanf <135324634+Rowdyvanf@users.noreply.github.com>
2024-12-05 11:57:22 +0100

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dikw-accelerator',
-    version='0.7',
+    version='0.1.8',
     packages=find_packages(where='lib'),
     package_dir={'': 'lib'},
     install_requires=[
