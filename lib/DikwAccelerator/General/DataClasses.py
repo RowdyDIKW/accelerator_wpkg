35bb57c2460ffe58b7690ffcb242d830f813981c
Rowdyvanf <135324634+Rowdyvanf@users.noreply.github.com>
2024-12-05 14:25:33 +0100

    version 0.1.11 create schema if not exists

--- lib/DikwAccelerator/General/DataClasses.py
+++ lib/DikwAccelerator/General/DataClasses.py
@@ -4,6 +4,7 @@ from DikwAccelerator.General.LakehouseUtils import LakehouseUtils
 from pyspark.sql import SparkSession
 from DikwAccelerator.General.GeneralUtils import print_with_current_datetime
 from icecream import ic
+from loguru import logger
 
 @dataclass
 class Schema:
@@ -21,7 +22,7 @@ class Schema:
         if not debug:
             ic.disable()
         ic()
-        print_with_current_datetime(f'saving dataset: {self.name}')
+        logger.info(f'saving dataset schema: {self.name}')
         if self.key_columns is None:
             self.lh_obj.save_tables(self.dataset,schema=self.name, debug=debug)
         else:
